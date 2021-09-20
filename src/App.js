49c93d399242a1d07e6b785650dcf2c6d14c7a77
Willie Jones <williejones@WilliesookM1Pro.attlocal.net>
2021-09-20 12:33:36 -0700

    Broken our homepage into smaller components, finished styling our menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import './homepage.styles.scss';
+import './pages/homepage/homepage.styles.scss';
 
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
